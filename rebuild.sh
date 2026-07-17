651e554687d7cc8152908d8a2ff6e87732b4158b
Sanket Parab <sanketsp@gmail.com>
2026-07-16 20:52:57 -0700

    fix(rebuild): add -b backup to Linux home-manager switch to avoid file conflicts

--- rebuild.sh
+++ rebuild.sh
@@ -5,5 +5,5 @@ ln -sfn "$DIR" ~/.dotfiles
 if [ "$(uname)" = "Darwin" ]; then
   exec sudo darwin-rebuild switch --flake ~/.dotfiles#mac
 else
-  exec nix run --extra-experimental-features "nix-command flakes" github:nix-community/home-manager/release-26.05 -- switch --flake ~/.dotfiles#ubuntu
+  exec nix run --extra-experimental-features "nix-command flakes" github:nix-community/home-manager/release-26.05 -- switch -b backup --flake ~/.dotfiles#ubuntu
 fi
